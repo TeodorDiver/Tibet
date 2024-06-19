cb3046a8c755858ebfed5dec0eb29c1f9130e735
Sergey Satarov <satarovsergej@gmail.com>
2024-06-19 21:25:39 +0400

    2

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,12 @@ export default function Header() {
     return (
         <header>
             <div>
-                <span className={styles.logo}>Coffe Shop</span>
+                <span className={styles.logo}>Тибет</span>
+                <ul className={styles.nav}>
+                    <li>О нас</li>
+                    <li>Контакты</li>
+                    <li>Личный кабинет</li>
+                </ul>
             </div>
             <div className={styles.presentation}></div>
         </header>
